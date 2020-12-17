653128258f226fe9fe1ad83abb95182019e03ff6
unknown <samer.ghaly2017@gmail.com>
2020-12-18 00:23:34 +0200

    courseAssignmentSchema

--- configurations/DBconfig.js
+++ configurations/DBconfig.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 
 const connectDB = () => {
+  console.log("process.env.MONGODB_URI")
   mongoose
     .connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
